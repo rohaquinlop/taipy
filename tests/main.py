340b37f974d2bbd0a26b04ed2304f993ba99c5bb
Robin Quintero <rohaquinlop301@gmail.com>
2024-03-19 00:42:57 -0500

    feat(test): add test for rust module
    - Add a test for the rust module

---
+++ tests/main.py
@@ -0,0 +1,9 @@
+from ty_py import (
+    rust,
+)
+import unittest
+
+
+class TestRust(unittest.TestCase):
+    def test_sum_as_string(self):
+        self.assertEqual(rust.sum_as_string(1, 2), "3")
